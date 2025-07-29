01d048311dc7ed3baa4901ccb1a58e86c5038501
Horia Manea <hmanea@c4sam.com>
2025-07-29 13:44:44 +0300

    feat(styledComponents, react icons): implement styled componets and react icons for design

--- src/ui/Heading.jsx
+++ src/ui/Heading.jsx
@@ -1,16 +1,33 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-const test = `text-align: center`;
+// const test = css`
+//   text-align: center;
+//   ${10 > 5 && "background-color: yellow"}
+// `;
 
-const Header = styled.h1`
-  font-size: 20px;
-  font-weight: 600;
-  background-color: yellow;
-  ${test}
+const Heading = styled.h1`
+  ${(props) =>
+    props.as === "h1" &&
+    css`
+      font-size: 3rem;
+      font-weight: 600;
+    `}
+
+  ${(props) =>
+    props.as === "h2" &&
+    css`
+      font-size: 2rem;
+      font-weight: 600;
+    `}
+    
+    ${(props) =>
+    props.as === "h3" &&
+    css`
+      font-size: 2rem;
+      font-weight: 500;
+    `}
+    
+  line-height: 1.4;
 `;
-
-function Heading() {
-  return <div></div>;
-}
 
 export default Heading;
